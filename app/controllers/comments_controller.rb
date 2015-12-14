9f56df75f9f49034d008a5788f47d71393db46fe
marijnkoel <marijnkoel@live.nl>
2015-12-14 11:50:51 +0100

    added comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+  def create
+    if params[:topic_id].present?
+      @topic = Topic.find(params[:topic_id])
+      @comment = @topic.comments.build(comment_params)
+
+      if @comment.save
+        redirect_to @topic,
+        notice: 'comment was created'
+      else
+        redirect_to @topic,
+        alert: 'Error creating comment'
+        @comment.errors.full_messages.to_sentence
+      end
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.find(params[:id])
+
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @topic, notice: 'Comment was destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+private
+
+
+  def comment_params
+    params.require(:comment).permit(:user, :text)
+  end
+end

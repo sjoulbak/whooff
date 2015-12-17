fc7a0f0b1cc3194acd171da2a1645fcc9de0b230
sjoulbak <remco_meinen@hotmail.com>
2015-12-17 11:25:58 +0100

    Added user_id to forms

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -70,6 +70,6 @@ class TopicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def topic_params
-      params.require(:topic).permit(:title, :text)
+      params.require(:topic).permit(:title, :text, :user_id)
     end
 end

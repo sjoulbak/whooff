b6dfe442ab4ee327a81d2c2d90257764ce47365b
marijnkoel <marijnkoel@live.nl>
2015-12-17 11:34:01 +0100

    created new view and controller and modified routes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  before_action :set_group, only: [:show]
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'topics#index'
+  root 'home#index'
   resources :user_sessions
   resources :users
   resources :faq
+  resources :home
 
   get '/login' => 'user_sessions#new', :as => :login
   get '/logout' => 'user_sessions#destroy', :as => :logout
